07f3eb1d030cddac77b3800655494ced22c7563b
DJ <djmckay@mac.com>
2018-07-26 10:38:28 -0400

    Initial Commit

---
+++ DJColorWheel/DJColorWheel.h
@@ -0,0 +1,19 @@
+//
+//  DJColorWheel.h
+//  DJColorWheel
+//
+//  Created by DJ McKay on 7/26/18.
+//  Copyright © 2018 DJ McKay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DJColorWheel.
+FOUNDATION_EXPORT double DJColorWheelVersionNumber;
+
+//! Project version string for DJColorWheel.
+FOUNDATION_EXPORT const unsigned char DJColorWheelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJColorWheel/PublicHeader.h>
+
+
